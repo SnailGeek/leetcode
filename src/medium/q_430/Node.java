7f86e1bf93d638bd5aee3e1288fbac33c3b94dba
3E-6-2-80  王帆(10023366) <wangf-q@glodon.com>
2021-01-22 21:32:11 +0800

    430 半成品

---
+++ src/medium/q_430/Node.java
@@ -0,0 +1,28 @@
+package medium.q_430;
+
+import java.util.List;
+
+public class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public static Node generateNodeList(List<Integer> nums) {
+        Node head = new Node(1);
+        for (int i = 0; i < nums.size(); i++) {
+            Node pNode = new Node(nums.get(i));
+            head.next = pNode;
+            if (i == 1) {
+                pNode.child = new Node(9);
+                pNode.child.next = new Node(10);
+            }
+            pNode = pNode.next;
+        }
+        return head;
+    }
+};
